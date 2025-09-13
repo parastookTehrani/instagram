109033f14bb30db3a31d195a4bebb46fbbb2d953
amir-es85 <amirfname@gmail.com>
2025-09-13 10:30:12 +0330

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+import {BrowserRouter as Router, Routes , Route } from 'react-router-dom'
 function App() {
 
+  
   return (
     <>
+  <Router>
+<Routes>
+  <Route path='/' element = ></Route>
+  <Route path='/signin' element = ></Route>
+  <Route path='/instagram' element = ></Route>
+</Routes>
+
+  </Router>
     </>
   )
 }
