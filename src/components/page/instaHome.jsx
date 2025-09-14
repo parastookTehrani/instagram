a682307e43cd432991bbeff3e2f0c5ffc1096af6
took <poti.tehrani@gmail.com>
2025-09-14 08:55:58 +0330

    first commit of day 2

--- src/components/page/instaHome.jsx
+++ src/components/page/instaHome.jsx
@@ -1,15 +1,11 @@
-
-import Post from '../Post'
+import Post from "../Post";
 import { InstaLink } from "../instlink";
 
-export function InstaHome (){
-
-    return (
-
-        <div className="flex">
-        <InstaLink />
+export function InstaHome() {
+  return (
+    <div className="flex">
+      <InstaLink />
       <Post />
-        </div>
-
-    )
+    </div>
+  );
 }
